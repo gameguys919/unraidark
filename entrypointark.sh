e70f815a7cec923a24ab2ebf5f22e1dc97283f7b
gameguys919 <73750584+gameguys919@users.noreply.github.com>
2024-07-23 19:06:32 +0200

    Update entrypointark.sh

--- entrypointark.sh
+++ entrypointark.sh
@@ -11,7 +11,7 @@ mkdir /data/serverfiles/ShooterGame/
 mkdir /data/serverfiles/ShooterGame/Saved/
 mkdir /data/serverfiles/ShooterGame/Saved/Config/
 mkdir /data/serverfiles/ShooterGame/Saved/Config/LinuxServer/
-wget "$_default" -O /data/config-lgsm/arkserver/common.cfg
+wget "$_default" -O /data/config-lgsm/arkserver/arkserver.cfg
 wget "$GameUserSettings" -O /data/serverfiles/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini
 wget "$Game" -O /data/serverfiles/ShooterGame/Saved/Config/LinuxServer/Game.ini
 sleep 15
