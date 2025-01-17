6d76596bae3b0aa2af1b1d6a0165147bcfb04ec7
gameguys919 <73750584+gameguys919@users.noreply.github.com>
2025-01-17 01:03:39 +0100

    Update blueprintinst.sh

--- blueprintinst.sh
+++ blueprintinst.sh
@@ -6,12 +6,18 @@ FILE="/var/www/html/blueprint.sh"
 # Check if the file does not exist
 if [ ! -f "$FILE" ]; then
     echo "File $FILE does not exist. Executing the script..."
-    microdnf install -y ca-certificates curl gnupg nano wget git zip findutils
     curl -fsSL https://rpm.nodesource.com/setup_20.x | bash - && \
     microdnf install -y nodejs && \
     microdnf clean all
     npm i -g yarn
     microdnf install -y unzip
+    microdnf install -y ca-certificates
+    microdnf install -y gnupg
+    microdnf install -y nano
+    microdnf install -y wget
+    microdnf install -y git
+    microdnf install -y zip
+    microdnf install -y findutils
     wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
     unzip -o release.zip
     echo -e "DOCKER='n'\nFOLDER='/var/www/html'" > .blueprintrc
