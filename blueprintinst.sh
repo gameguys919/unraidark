bc168b6ba18ca406ea77759e31c8bb34eb0c4a69
gameguys919 <73750584+gameguys919@users.noreply.github.com>
2025-01-17 01:00:44 +0100

    Update blueprintinst.sh

--- blueprintinst.sh
+++ blueprintinst.sh
@@ -6,23 +6,21 @@ FILE="/var/www/html/blueprint.sh"
 # Check if the file does not exist
 if [ ! -f "$FILE" ]; then
     echo "File $FILE does not exist. Executing the script..."
-    
-    # Place the commands you want to execute here
-    # Example: Create the blueprint.sh file or other tasks
-    # touch "$FILE"  # Uncomment if you want to create the file
-
-else
-    microdnf install -y ca-certificates curl gnupg nano wget git zip unzip findutils
+    microdnf install -y ca-certificates curl gnupg nano wget git zip findutils
     curl -fsSL https://rpm.nodesource.com/setup_20.x | bash - && \
     microdnf install -y nodejs && \
     microdnf clean all
     npm i -g yarn
+    microdnf install -y unzip
     wget "$(curl -s https://api.github.com/repos/BlueprintFramework/framework/releases/latest | grep 'browser_download_url' | cut -d '"' -f 4)" -O release.zip
-    unzip release.zip
+    unzip -o release.zip
     echo -e "DOCKER='n'\nFOLDER='/var/www/html'" > .blueprintrc
     chmod +x blueprint.sh
     yarn install
     ./blueprint.sh
     adduser --user-group www-data
     chmod -R 777 /var/www/html
+    # Place the commands you want to execute here
+    # Example: Create the blueprint.sh file or other tasks
+    # touch "$FILE"  # Uncomment if you want to create the file
 fi
